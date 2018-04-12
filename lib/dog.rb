6da4ca622fcdd062e18469beeda6fd8a565bebd5
Joyce Chun <wooyangee@gmail.com>
2018-04-12 22:25:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,18 @@
 class Dog 
-  def initialize(dog_name)
+  def initialize(dog_name, dog_breed)
     @name = dog_name
+    @breed = dog_breed
   end
   
   def name
     @name
   end
 
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def breed
+    @breed
+  end
 end
